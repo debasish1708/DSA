32c1b0bac3797e44f018e53f24bebac77734fd5f
debasish1708 <debasishdas1708@gmail.com>
2025-06-04 22:13:31 +0530

    working on document distance

--- DocumentDistance.java
+++ DocumentDistance.java
@@ -4,17 +4,34 @@ import java.util.Scanner;
 
 public class DocumentDistance {
     public static void main(String[] args) {
+        /*
+         * Document Distance Algorithm
+         * 
+         */
         try {
             File document1=new File("./Document/D1.txt");
-            Scanner doc1_reader=new Scanner(document1);
+            File document2=new File("./Document/D2.txt");
+            Scanner doc1_reader=new Scanner(document1,"UTF-8");
+            Scanner doc2_reader=new Scanner(document2,"UTF-8");
+
             while (doc1_reader.hasNextLine()) {
                 String data=doc1_reader.nextLine();
                 System.out.println(data);
             }
+
+            while(doc2_reader.hasNextLine()){
+                String data=doc2_reader.nextLine();
+                System.out.println(data);
+            }
+
             doc1_reader.close();
+            doc2_reader.close();
+
         } catch (FileNotFoundException e) {
             System.out.println("An error is Occurred");
             e.printStackTrace();
+        } catch(Exception e){
+            System.out.println(e.getMessage());
         }
         
     }

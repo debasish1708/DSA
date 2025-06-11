42a2559083bdf23b9534bf465b4291dfd218fb16
debasish1708 <debasishdas1708@gmail.com>
2025-06-11 23:52:42 +0530

    change t in place of dp

--- Knapsack.java
+++ Knapsack.java
@@ -16,18 +16,18 @@ public class Knapsack {
     }
 
     // 0-1 knapsack solution (Memoization)
-    private static int[][] dp;
+    private static int[][] t;
     private static int knapsackMemo(int[] wt, int[] val, int W, int n) {
         if (n == 0 || W == 0) return 0;
-        if (dp[n][W] != -1) return dp[n][W];
+        if (t[n][W] != -1) return t[n][W];
 
         if (wt[n - 1] <= W) {
             int yes = val[n - 1] + knapsackMemo(wt, val, W - wt[n - 1], n - 1);
             int no = knapsackMemo(wt, val, W, n - 1);
-            return dp[n][W] = Math.max(yes, no);
+            return t[n][W] = Math.max(yes, no);
         } 
 
-        return dp[n][W] = knapsackMemo(wt, val, W, n - 1);
+        return t[n][W] = knapsackMemo(wt, val, W, n - 1);
     }
     public static void main(String[] args) {
         int[] wt = {1, 3, 4, 5};
@@ -35,14 +35,14 @@ public class Knapsack {
         int W = 7;
 
         System.out.println(knapsackRec(wt, val, W, 4)); // not optimal solution
-        dp = new int[4 + 1][W + 1];
+        t = new int[4 + 1][W + 1];
         for (int i = 0; i <= 4; i++) {
-            Arrays.fill(dp[i], -1);
+            Arrays.fill(t[i], -1);
         }
         System.out.println(knapsackMemo(wt, val, W, 4)); // optimal solution using memoization
         for(int i = 0; i <= 4; i++) {
             for (int j = 0; j <= W; j++) {
-                System.out.print(dp[i][j] + " ");
+                System.out.print(t[i][j] + " ");
             }
             System.out.println();
         }

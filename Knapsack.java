217382a516c8129dba54ebd4d668ea12439f00fc
debasish1708 <debasishdas1708@gmail.com>
2025-06-12 22:25:30 +0530

    learning Tabulation in DP implement Top Down Approach for 0/1 Knapsack in DP

--- Knapsack.java
+++ Knapsack.java
@@ -29,6 +29,26 @@ public class Knapsack {
 
         return t[n][W] = knapsackMemo(wt, val, W, n - 1);
     }
+    // Tabulation in DP for knapsack
+    private static int knapsackTopDown(int W, int val[], int wt[]) {
+        // code here
+        int n = val.length;
+        int[][] t = new int[n + 1][W + 1];
+        // initialization
+        for(int i=0;i<n+1;i++) t[i][0]=0;
+        for(int j=0;j<W+1;j++) t[0][j]=0;
+        for (int i=1;i<n+1;i++) {
+            for (int j=1;j<W+1;j++) {
+                if (wt[i-1]<=j) {
+                    t[i][j]=Math.max(val[i-1]+t[i-1][j-wt[i-1]], t[i-1][j]);
+                } else {
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+    
+        return t[n][W];
+    }
     public static void main(String[] args) {
         int[] wt = {1, 3, 4, 5};
         int[] val = {1, 4, 5, 7};
@@ -46,5 +66,7 @@ public class Knapsack {
             }
             System.out.println();
         }
+        System.out.println("Tablulation in DP");
+        System.out.println(knapsackTopDown(W, val, wt));
     }
 }
